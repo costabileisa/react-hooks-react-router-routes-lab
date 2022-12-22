2f62d6f2f0d8c649710ab9f5744e0fbbc58c26ec
Isabella Costabile <costabileisa@gmail.com>
2022-12-22 10:30:27 -0500

    commit tree a8fc4ec641b00351d6ae8580df5f45ba0a6cbb3b

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const getDirectors = directors.map(director => {
+    const listMovies = director.movies.map(movie => {
+      return (
+        <li key={movie}>{movie}</li>
+      )
+    })
+    return (
+      <div key={director.name}>
+        <h3>{director.name}</h3>
+        <ul>
+          {listMovies}
+        </ul>
+      </div>
+    )
+  })
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {getDirectors}
+  </div>
+  )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,30 @@
 import React from "react";
 import { movies } from "../data";
 
+const getMovies = movies.map(movie => {
+  const listGenres = movie.genres.map(genre => {
+    return (
+      <li key={movie[genre]}>{genre}</li>
+    )
+  })
+  return (
+    <div key={movie.title}>
+      <h3>{movie.title}</h3>
+      <small>{movie.time} Minutes</small>
+      <ul>
+        {listGenres}
+      </ul>
+    </div>
+  )
+})
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {getMovies}
+  </div>
+  )
 }
 
 export default Movies;
